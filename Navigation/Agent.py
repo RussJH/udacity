313f2554ff145650d17d34a2eae31a29f62f2566
RussJH <russell.j.harrington@gmail.com>
2021-04-11 01:13:05 -0400

    Add minor updates for structure

---
+++ Navigation/Agent.py
@@ -0,0 +1,25 @@
+
+
+class Agent():
+    """Deep Q-learning agent 
+    """
+
+    def __init__(self):
+        """
+        TODO: Add init code here
+        """
+
+    def learn(self):
+        """
+        TODO: implement learning
+        """
+
+    def update(self):
+        """
+        TODO: implement updates
+        """
+    
+    def get_action(self):
+        """
+        TODO: implement action
+        """
